e5ab2cde446224c236c36a637becc267ce11648a
dilip <mihirsingh241@gmail.com>
2024-07-11 17:26:47 +0530

    in deployment the server is not working corectly it is crashing os fix it as it not require session storage so remove from the file

--- app.js
+++ app.js
@@ -3,24 +3,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const session = require('express-session');
 const cors = require('cors')
 const flash = require('connect-flash');
-const cloudinary = require('cloudinary').v2
 require('dotenv').config()
 
 var indexRouter = require('./routes/index');
 
 var app = express();
 app.use(cors());
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use(session({
-  resave:false,
-  saveUninitialized:true,
-  secret:'lolopolo'
-}));
 app.use(flash())
 
 
@@ -32,15 +22,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
-
-          
-cloudinary.config({ 
-  cloud_name: process.env.CLOUD_NAME, 
-  api_key: process.env.API_KEY, 
-  api_secret: process.env.API_SECRET 
-});
-
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
